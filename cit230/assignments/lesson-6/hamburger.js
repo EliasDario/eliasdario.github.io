79f47e70a12443f23a36a638b4831e3a64ae8427
Elías <35237918+EliasDario@users.noreply.github.com>
2018-03-07 17:57:50 -0300

    Lesson 7 upload

--- cit230/assignments/lesson-6/hamburger.js
+++ cit230/assignments/lesson-6/hamburger.js
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
 function toggleMenu() {
     console.log(document.getElementById("primaryNav").classList);
     document.getElementById("primaryNav").classList.toggle("hide");
-=======
-function toggleMenu() {
-    console.log(document.getElementById("primaryNav").classList);
-    document.getElementById("primaryNav").classList.toggle("hide");
->>>>>>> fe87975f29506faf93e0bb6ed989df1262808f97
 }
