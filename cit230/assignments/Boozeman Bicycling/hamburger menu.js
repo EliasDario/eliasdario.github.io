10261b1df35a54955ef767b16f9cb147879624b9
Elías <35237918+EliasDario@users.noreply.github.com>
2018-04-16 22:43:53 -0300

    Add files via upload

--- cit230/assignments/Boozeman Bicycling/hamburger menu.js
+++ cit230/assignments/Boozeman Bicycling/hamburger menu.js
@@ -17,5 +17,5 @@ function showSlides() {
     slideIndex++;
     if (slideIndex > slides.length) {slideIndex = 1} 
     slides[slideIndex-1].style.display = "block"; 
-    setTimeout(showSlides, 7000); // Change image every 10 seconds
+    setTimeout(showSlides, 5000); // Change image every 10 seconds
 }
